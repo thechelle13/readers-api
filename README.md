c2cf7d5c028df864f9d73688cd8bc74ee9c19cb6
Michelle Totherow <tym4me13@gmail.com>
2023-11-09 08:02:47 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Starting your Reader's Digest Django Project
+In your terminal, go to your workspace directory.
+Create a new directory called readers-digest.
+cd to that directory.
+You are going to run a script that prompts you for a word for your project. When it prompts you, enter the word digest.
+
+Once the process completes, run pipenv shell to activate the virtual environment that was created for your project.
+Open the project in Visual Studio Code.
---
+++ digestproject/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from digestapi.views import BookViewSet, UserViewSet, CategoryViewSet, ReviewViewSet
+
+router = DefaultRouter(trailing_slash=False)
+router = DefaultRouter(trailing_slash=False)
+router.register(r'books', BookViewSet, 'book')
+router.register(r'categories', CategoryViewSet, 'category')
+router.register(r'reviews', ReviewViewSet, 'review')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('login', UserViewSet.as_view({'post': 'user_login'}), name='login'),
+    path('register', UserViewSet.as_view({'post': 'register_account'}), name='register'),
+]
